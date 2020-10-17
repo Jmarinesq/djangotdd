c170f44a5f55756a23fc95760d5f6fad01e2d99a
juan.marin <juan.marin@axa.es>
2020-10-17 15:58:56 +0200

    added List model, 2 migrations

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 from django.http import request
 import django
 from django.http import HttpResponse
@@ -16,3 +16,8 @@ def home_page(request: django.http.request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
