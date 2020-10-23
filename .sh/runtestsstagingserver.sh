67d72a997ac09c115c1b52a1aae5e8337ea22afe
juan.marin <juan.marin@axa.es>
2020-10-23 20:46:58 +0200

    deleted static folder

---
+++ .sh/runtestsstagingserver.sh
@@ -0,0 +1,2 @@
+STAGING_SERVER=obeythegoat2.azurewebsites.net/ /Users/A187AG/Desktop/knwldg/Programming/djangoTDD//manage.py test functional_tests \
+    --failfast
--- .sh/stagingServer.sh
+++ .sh/stagingServer.sh
@@ -1,5 +1,5 @@
-virtualenv ENV
-source ENV/bin/activate
+source /antenv/bin/activate
+/antenv/bin/python /home/site/wwwroot/manage.py migrate --noinput
 apt-get install git -y
 git clone https://github.com/Jmarinesq/djangotdd.git .
 
