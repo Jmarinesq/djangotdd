37cf5f0b2ce7aa05fe76660bd084d0eb658b9bbe
juan.marin <juan.marin@axa.es>
2020-10-23 23:14:40 +0200

    fixed test for staging server

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -78,7 +78,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        print("here 1")
         edith_list_url = self.browser.current_url
+        print(f'edith_list_url{edith_list_url}')
+        time.sleep(4)
         response = self.client.get(edith_list_url)
         self.browser.quit()
 
@@ -87,7 +90,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
         if staging_server:
             self.live_server_url = f'http://{staging_server}'
 
+        print(f'List to open: {response.context["list"].id}')
         self.client.get(f'/lists/{response.context["list"].id}/')
+        print("here 2")
         # She visits that URL - her to-do list is still there.
         self.browser.get(edith_list_url)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
