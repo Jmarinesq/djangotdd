6f59ddfb00bee57a258a91f996a694959e633c2b
juan.marin <juan.marin@axa.es>
2020-10-30 16:08:39 +0100

    Added ExistingFormItem with item duplication validation

--- lists/forms.py
+++ lists/forms.py
@@ -24,9 +24,10 @@ class ItemForm(forms.models.ModelForm):
         return super().save()
 
 
-class ExistingListItemForm(forms.models.ModelForm):
+class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.instance.list = for_list
 
     def validate_unique(self):
         try:
@@ -34,4 +35,7 @@ class ExistingListItemForm(forms.models.ModelForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
 
