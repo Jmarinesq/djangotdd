781ccf5b10fe806ee15209a565e6e9e6684fe101
juan.marin <juan.marin@axa.es>
2020-11-01 18:36:31 +0100

    creating preauthenticated session in server

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -22,5 +22,5 @@ def create_session_on_server(host, email):
     with settings(host_string=f'jmarin@{host}'):
         env_vars = _get_server_env_vars(host)
         with shell_env(**env_vars):
-            session_key = run(f'{manage_dot_py} create session {email}')
+            session_key = run(f'{manage_dot_py} create_session {email}')
             return session_key.strip()
