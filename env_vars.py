2c0a48ce10efd85c8b5833ad226b2f6941b060bc
juan.marin <juan.marin@axa.es>
2020-11-06 20:45:14 +0100

    setting chromedriver path

--- env_vars.py
+++ env_vars.py
@@ -1,8 +1,12 @@
 import os
 
 
-chromedriver_path = "/Users/A187AG/Desktop/knwldg/Programming/djangoTDD/chromedriver"
 
 def set_env_vars():
-    os.environ["PATH"] = os.environ["PATH"] + ":/Users/A187AG/Desktop/knwldg/Programming/djangoTDD"
+    chromedriver_path = os.environ['CHROMEDRIVER_PATH']
+    if not chromedriver_path:
+        chromedriver_path = "/Users/A187AG/Desktop/knwldg/Programming/djangoTDD/chromedriver"
+        os.environ["PATH"] = os.environ["PATH"] + ":/Users/A187AG/Desktop/knwldg/Programming/djangoTDD"
+    else:
+        os.environ["PATH"] = os.environ["PATH"] + ":/usr/bin/"
     os.environ["DJANGO_SETTINGS_MODULE"] = "superlists.settings"
